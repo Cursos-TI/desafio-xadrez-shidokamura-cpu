a6229f6f14665874042200abd9ae3b0f3ab72764
Matheus <shidokamura@gmail.com>
2026-05-29 14:07:55 +0000

    Desafio xadrez Concluido. Foram muitos desafios quebrei o codigo milhares de vezes tentando coisas diferentes. tentei fazer um menu de direções mas desisti porque quebrava tudo, talvez usando alguma coisa que não conheço ainda funcionasse.

--- xadrez.c
+++ xadrez.c
@@ -1,144 +1,99 @@
 #include <stdio.h>
 
-// Nível Mestre - Funções Recursivas e Loops Aninhados
+// Nível Mestre - Funções Recursivas e Loops Aninhados 
 void movimentoRecursivoBispo(int bispo) {  //função recursiva
-    if (bispo > 0 && bispo <= 6) {  // Movimento recursivo do Bispo.
-        printf("O Bispo tem %d movimentos.\n", bispo);
-        movimentoRecursivoBispo(bispo - 1); // o Bispo anda.     
+    if (bispo >= 1 && bispo < 6) {  // Movimento recursivo do Bispo.
+        bispo --;
+        printf("O Bispo se move para a diagonal superior direita\n", bispo); 
+        movimentoRecursivoBispo(bispo);  // o Bispo anda.    
     }    
 }
+
 void movimentoRecursivoTorre(int torre) {  //função recursiva
-    if (torre > 0 && torre <= 6) {  // Movimento recursivo do Bispo.
-        printf("A Torre tem %d movimentos.\n", torre);
-        movimentoRecursivoTorre(torre - 1); // o Bispo anda.     
+    if (torre >= 1 && torre < 6) {  // Movimento recursivo da Torre.
+        torre --;
+        printf("A Torre se move para a direita\n", torre);  
+        movimentoRecursivoTorre(torre); // a Torre anda.    
     }    
 }
+
 void movimentoRecursivoRainha(int rainha) {  //função recursiva
-    if (rainha > 0 && rainha <= 6) {  // Movimento recursivo do Bispo.
-        printf("A Rainha tem %d movimentos.\n", rainha);
-        movimentoRecursivoRainha(rainha - 1); // o Bispo anda.     
+    if (rainha >= 1 && rainha < 9) {  // Movimento recursivo da Rainha.
+        rainha --;
+        printf("A Rainha se move para a esquerda\n", rainha); 
+        movimentoRecursivoRainha(rainha); // a Rainha anda.  
+          
     }    
 }
 
-
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    
+int main() { 
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int pecas, bispo, torre, rainha, cavaloMove1, cavaloMove2; // peças  
-    int direcao;
+    int pecas, bispo, torre, rainha, cavaloMove1, cavaloMove2; // peças 
 
-    bispo = 0;
-    torre = 0;
-    rainha = 0;
-    cavaloMove1 = 0;
-    cavaloMove2 = 0;
-
-    do {  // Inicializa o App
+    do { //Menu basico né, sempre retorna aqui depois de um bloco de codigo switch ser executado.
         printf("***Conhecendo peças de Xadrez***\n");
-        //printf("Escolha uma direção:\n7.🡔 | 8.🡑 | 9.🡕\n4.🡐  Peça   6.🡒\n1.🡗 | 2.🡓 | 3.🡖\n");
-        printf("Peças\n1. Bispo\n2. Torre\n3. Rainha\n4. Cavalo\n0.Sair\n");
-        scanf("%d", &pecas);
-            switch (pecas) {
-            case 1: // Implementação de Movimentação do Bispo
-            printf("Você escolheu o Bispo\n");
-            printf("Quantas casas o Bispo deve se mover? \n");
-            scanf("%d", &bispo);
-            while (bispo > 0 && bispo <6) {
-                movimentoRecursivoBispo(bispo);
-            } 
-            if (bispo == 0) {
-                printf("O Bispo terminou a jogada\n");
-            } else if (bispo > 6) {
-                printf("O Bispo não pode andar mais que 5 casas\n");
-                printf("Quantas casas o Bispo deve se mover? \n");
-            }    
-                break;
-            case 2: // Implementação de Movimentação da Torre
-            printf("Você escolheu a Torre\n");
-            printf("Quantas casas a Torre deve se mover?\n");
-            scanf("%d", &torre);
-            do {
-                //menu_direcoes;
-                movimentoRecursivoTorre(torre); // lembra de colocar pra direita 
-            } while (torre > 0 && torre < 5);
-            if (torre == 0) {
-                printf("A Torre terminou a jogada\n");
-            } else if (torre > 5) {
-                printf("A Torre não pode andar mais que 5 casas\n");
-                printf("Quantas casas a Torre deve se mover? \n");
-            }
+        printf("Peças\n1. Bispo\n2. Torre\n3. Rainha\n4. Cavalo\n0.Sair\n"); // apresento as peças pra escolher e seus numeros no menu.
+        scanf("%d", &pecas); // usuario escolhe a peça
+            switch (pecas) { 
+            case 1: // Implementação de Movimentação do Bispo                
+                printf("Você escolheu o Bispo\n");
+                printf("Quantas casas o bispo deve se mover? \n");
+                scanf("%d", &bispo);                 
+                    while (bispo > 5) {
+                        printf("O Bispo não pode andar mais que 5 casas\n");
+                        break;
+                        }
+                movimentoRecursivoBispo(bispo); 
+                    printf("O Bispo terminou a jogada\n");
+                    printf("\n");
             break;
+                
+            case 2: // Implementação de Movimentação da Torre
+                printf("Você escolheu a Torre\n");
+                printf("Quantas casas a Torre deve se mover?\n");
+                scanf("%d", &torre);  
+                while (torre > 5) { // era um do-while aqui mas por algum motivo tava dando loop infinito então eu tirei tudo e simplifiquei
+                    printf("A Torre não pode andar mais que 5 casas\n");
+                    break;
+                 }
+                movimentoRecursivoTorre(torre);
+                    printf("A Torre terminou a jogada\n");
+                    printf("\n");
+                break;
             case 3: // Implementação de Movimentação da Rainha
                 printf("Você escolheu a Rainha\n");
-                printf("Quantas casas a rainha deve se mover? \n");
+                printf("Quantas casas a rainha deve se mover?\n");
                 scanf("%d", &rainha);
-                for (rainha = 0; rainha <= 8 ; rainha ++) {
-                    //menu_direcoes(1);
-                    movimentoRecursivoRainha(rainha); // lembra de colocar pra esquerda                     
-                    if (rainha == 0) {
-                        printf("A Rainha terminou a jogada\n");
-                    } else if (rainha > 5) {
-                        printf("A Rainha não pode andar mais que 5 casas\n");
-                        printf("Quantas casas a Rainha deve se mover? \n");
-                    break;
-                    }
-                }
+                for (rainha ; rainha > 8;) { //feio demais assim mas funcionou XD foram muitas tentativas 
+                    printf("A Rainha não pode andar mais que 8 casas\n");
                 break;
+                }
+                movimentoRecursivoRainha(rainha);                              
+                    printf("A Rainha terminou a jogada\n");
+                    printf("\n");         
+                    break;
             case 4: // Implementação de Movimentação do Cavalo
                 printf("Você escolheu o Cavalo\n");
-                printf("O Cavalo fará apenas um movimento para cima e até 3 casas para a direita\n"); //ainda pensando em uma forma de fazer isso melhor.
-                printf("Quantas vezes o cavalo deve para direita? \n");
-                scanf("%d", &cavaloMove2);
-                while (cavaloMove1 <= 2 ) {
-                    for (cavaloMove2 = 0; cavaloMove2 < 3; cavaloMove1++) {
+                printf("O Cavalo fará apenas um movimento para cima e até 3 casas para a direita\n"); 
+                //ainda pensando em uma forma de fazer isso melhor. att: não achei forma melhor quanto mais eu mexia mais quebrava tudo.
+                for (cavaloMove1 = 0; cavaloMove1 < 1; cavaloMove1 ++) {
+                    for (cavaloMove2 = 0; cavaloMove2 <= 2; cavaloMove2 ++) {
                         printf("Cavalo anda para direita\n");
                     }
                     printf("Cavalo anda para cima\n");                
                 }
+                printf("O Cavalo terminou a jogada\n"); 
+                printf("\n");
                 break;
-            default:
+            case 0: // para finalizar o app
+                pecas = 0;
+                break;
+            default: // caso o usuario escolha um numero que não tenha peça.
                 printf("Não tem dama! XD\n");
+                printf("\n");
                 break;
-            }    
-            
-
-            // Implementação de Movimentação do Bispo
-            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-            
-            
-            // Implementação de Movimentação da Torre
-            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-            printf("Quantas casas a Torre deve se mover? \n");
-            scanf("%d", &torre);
-            do {
-                movimentoRecursivoTorre(torre); // lembra de colocar pra direita 
-            
-            } while (torre > 0 && torre < 5);
-            
-            // Implementação de Movimentação da Rainha
-            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-            for (rainha = 0; rainha <= 8 ; rainha ++) {
-            movimentoRecursivoRainha(rainha); // lembra de colocar pra esquerda  
-                
-            }
-            // Nível Aventureiro - Movimentação do Cavalo
-            // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-            // Um loop pode representar a movimentação horizontal e outro vertical.
-            while (cavaloMove1 < 2) {
-                for (cavaloMove2 = 0; cavaloMove2 < 3; cavaloMove1++, cavaloMove2++) {
-                    printf("Cavalo anda para cima\n");
-                    }
-                printf("Cavalo anda para direita\n");                
-            } 
-        // Sugestão: Substitua as movimentações das peças por funções recursivas.
-
-        // Inclua o uso de continue e break dentro dos loops.     
-        } while (pecas != 0); // Finaliza o App.
+        }
+    } while (pecas != 0); // Finaliza o App.
     printf("Fim do jogo...\n"); 
     return 0;
 }
